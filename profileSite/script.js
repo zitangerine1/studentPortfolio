afa23ea3c392996f53436564f36ed30b6b054c37
zitangerine <hs190182@sji-international.com.sg>
2022-05-28 21:30:50 +0800

    28/05/2022 9:30PM final commit

--- profileSite/script.js
+++ profileSite/script.js
@@ -1,22 +1,9 @@
-mybutton = document.getElementById("toTopButton");
-window.onscroll = function() {scrollFunction()};
-
-destination = document.getElementsByClassName("personalIntroduction")
-console.log(destination)
-
-function toIntro() {
- destination.scrollIntoView();
- console.log('Function has executed')
-}
-
-function scrollFunction() {
- if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-  mybutton.style.display = "block";
- } else {
-  mybutton.style.display = "none";
- }
-}
-
 function topFunction() {
  document.documentElement.scrollTop = 0;
 }
+
+$("button").click(function() {
+ $('html,body').animate({
+    scrollTop: $(".personalIntroduction").offset().top},
+   'slow');
+});
