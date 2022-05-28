5dc14aa5a2ab1a5812efe594fb52589509a951e3
zitangerine <hs190182@sji-international.com.sg>
2022-05-28 13:03:02 +0800

    Adding more resources and starting Assessment
    - Added SJI logo

--- profileSite/script.js
+++ profileSite/script.js
@@ -1,5 +1,22 @@
-const introElement = document.getElementsByClassName('personalIntroduction')
+mybutton = document.getElementById("toTopButton");
+window.onscroll = function() {scrollFunction()};
 
-function scroll(introElement) {
- introElement.scrollIntoView()
+destination = document.getElementsByClassName("personalIntroduction")
+console.log(destination)
+
+function toIntro() {
+ destination.scrollIntoView();
+ console.log('Function has executed')
+}
+
+function scrollFunction() {
+ if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  mybutton.style.display = "block";
+ } else {
+  mybutton.style.display = "none";
+ }
+}
+
+function topFunction() {
+ document.documentElement.scrollTop = 0;
 }
